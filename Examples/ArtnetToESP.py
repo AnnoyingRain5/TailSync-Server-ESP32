d38efc10ddbbf4987348d7169734c1cc0ce4ebda
AnnoyingRains <annoyingrain5@gmail.com>
2026-01-14 13:42:49 +1100

    feat(ArtnetToESP): take port from arguments, update for latest libTailSync

--- Examples/ArtnetToESP.py
+++ Examples/ArtnetToESP.py
@@ -1,18 +1,26 @@
 # Example usage of the ESP TailLink server
 # Gets data from universe zero, and sends it over to taillink!
-# Channels will be interpreted as RGB values (in that order). 64 pixels are expected, for 192 channels total.
+# Channels will be interpreted as RGB values (in that order). 30 pixels are expected, for 90 channels total.
 # Other packet types are currently unimplimented.
 from stupidArtnet import StupidArtnetServer
 import time
 import subprocess
 import serial
+import sys
 
 UNIVERSE = 0
-PORT = "/dev/ttyACM0"
+try:
+    PORT = sys.argv[1]
+except IndexError:
+    print("Usage: python3 ArtnetToESP.py PATH/TO/SERIAL_PORT")
+    exit(1)
 BAUD_RATE = 460800
 
 packet = b""
-lastPacketRx = time.time()
+lastpacket = b""
+lastPacketTx = time.time()
+
+pulse = False
 
 
 def rcv_artnet(data):
@@ -20,7 +28,15 @@ def rcv_artnet(data):
     # the received data is an array
     # of the channels value (no headers)
     global packet
-    packet = bytes(b"\x01") + bytes(data[0:192])
+    print("packet from ART-NET!")
+    packet = bytes(b"\x01") + bytes(data[0:90])
+    global pulse
+    if data[90] == 255 and pulse == False:
+        ser.write(bytes(b"\x00"))
+        print(ser.readline().decode("utf-8"), end="")
+        pulse = True
+    else:
+        pulse = False
 
 
 try:
@@ -32,10 +48,12 @@ except serial.SerialException as e:
     sys.exit(1)
 
 srv = StupidArtnetServer()
-u1_listener = srv.register_listener(UNIVERSE, callback_function=rcv_artnet)
+listener = srv.register_listener(UNIVERSE, callback_function=rcv_artnet)
 
-# do nothing forever
 while True:
-    ser.write(packet)
-    print(ser.readline().decode("utf-8"), end="")
-    time.sleep(0.033)
+    if packet != lastpacket or time.time() - lastPacketTx > 2:
+        ser.write(packet)
+        print(ser.readline().decode("utf-8"), end="")
+        lastpacket = packet
+        lastPacketTx = time.time()
+        time.sleep(0.0165)
